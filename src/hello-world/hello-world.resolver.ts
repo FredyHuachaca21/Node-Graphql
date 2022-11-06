9864a950408f50b668b8f43c085ae5e0f29fd377
Fredy <fredy.huachaca21@gmail.com>
2022-11-06 16:25:27 -0500

    new query

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,10 +1,17 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
 
-    @Query(()=> String)
+    @Query(()=> String, {description: 'Retorna un string de Hola Mundo', name: 'Hello'})
     HelloWorld(): string{
         return 'Hola Mundo';
     }
+
+    @Query(()=> Float, {description: 'Retorna número Random', name: 'random'})
+    getRandomNumber(): number{
+        return Math.random() * 100;
+    }
+
+
 }
