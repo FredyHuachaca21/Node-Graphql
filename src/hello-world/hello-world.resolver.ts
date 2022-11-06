ccb49eb1b645791dea285c06c42805070bf22421
Fredy <fredy.huachaca21@gmail.com>
2022-11-06 17:24:03 -0500

    mew querys with args

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,4 +1,4 @@
-import { Float, Int, Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
@@ -13,10 +13,27 @@ export class HelloWorldResolver {
         return Math.random() * 100;
     }
 
-    @Query(()=> Int, {description: 'Retorna número  entre x & y', name: 'randomFromZeroTo'})
+    @Query(()=> Int, {description: 'Retorna número  entre cero & y', name: 'randomFromZeroTo'})
     getRandomFromZeroTo(): number{
         return Math.floor(Math.random() * 5);
     }
 
+    @Query(()=> Int, {description: 'Retorna número  entre cero(No excluyente) hasta arg(TO)', name: 'randomFromZeroToArg'})
+    getRandomFromZeroToArg(@Args('to') to: number): number{
+        return Math.floor(Math.random() * to);
+    }
+
+    @Query(()=> Int, {description: 'Retorna número  entre min (incluido) a max (excluido)', name: 'randomFromMinAndMax'})
+    getRandomFromArgtoArg(@Args('min') min: number, @Args('max') max: number,): number{
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    @Query(()=> Int, {description: 'Retorna número  entre min  a max (excluido ambos)', name: 'randomFromMinAndMaxExclude'})
+    getRandomFromArgtoArgExclude(@Args('min') min: number, @Args('max') max: number,): number{
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
 
 }
