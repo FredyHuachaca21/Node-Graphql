ef37febd0663a39b7317f532d44b80ddf225ca66
Fredy <fredy.huachaca21@gmail.com>
2022-11-06 15:32:22 -0500

    First Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    HelloWorldModule,
   ],
   controllers: [],
   providers: [],
